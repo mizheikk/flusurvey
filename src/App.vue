9ccb82dc9220ecd38dc259876ea6bc9dc7542daf
Petri Rautiainen <petri.raut@gmail.com>
2020-03-22 21:30:14 +0200

    Added simple survey form.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
     <div>
-        <h1>Hello World!</h1>
-        <p>Hotreloading works!</p>
+        <p>Onko flunssaoireita?</p>
+
+        <input type="radio" id="fluPositive" value="1" v-model="fluStatus">
+        <label for="fluPositive">Kyllä</label>
+
+        <input type="radio" id="fluNegative" value="0" v-model="fluStatus">
+        <label for="fluNegative">Ei</label>
+
+        <br />
+
+        <label for="age">Ikä</label>
+        <input type="number" id="age" step="1" v-model="age">
+
+        <br />
+
+        <label for="postalCode">Postinumero</label>
+        <input type="text" id="postalCode" v-model="postalCode">
     </div>
 </template>
 
 <script>
 export default {
-    name: 'App'
+    name: 'App',
+
+    data() {
+        return {
+            fluStatus: null,
+            age: null,
+            postalCode: null
+        }
+    }
 }
 </script>
